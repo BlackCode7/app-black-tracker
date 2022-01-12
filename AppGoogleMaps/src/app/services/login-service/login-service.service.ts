5432956a612e28691f4d5a99d9aeb9f8490fdc6b
Anderson <BlackCode7@outlook.com>
2022-01-11 21:31:02 -0300

    Acrecentando um método ao service para trazer os dados

--- AppGoogleMaps/src/app/services/login-service/login-service.service.ts
+++ AppGoogleMaps/src/app/services/login-service/login-service.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginServiceService {
-
+  
   constructor(private http: HttpClient) { }
+
+  baseUrl: string = 'http://localhost:3000'
+
+  public obterTabelaLogin(id: number):Observable<any[]> {
+    return this.http.get<any>(this.baseUrl+ "TB_Login" +id)
+  }
+
 }
--- AppGoogleMaps/src/app/tela-login/tela-login.component.ts
+++ AppGoogleMaps/src/app/tela-login/tela-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginModelDTO } from '../modelsDTO/login-modelDTO';
+import { LoginServiceService } from '../services/login-service/login-service.service'
 
 @Component({
   selector: 'app-tela-login',
@@ -9,9 +11,34 @@ export class TelaLoginComponent implements OnInit {
   component: any;
   title = "Test Title";
 
-  constructor() { }
+  login: any = [];
+  loginModelDTO = new LoginModelDTO();
+
+
+  constructor(
+    private loginServiceService: LoginServiceService
+  ) { }
+
+  obterTabelaLogin(id: any){
+    this.loginServiceService.obterTabelaLogin(id).subscribe( data => {
+      console.log("ERRO de retorno da SERVICE LOGIN ---> ",data);
+      if (data != null) {
+        this.login = data;
+        if ( this.login.data[0] != null ) {
+          this.loginModelDTO = this.login
+        }
+      }
+    })
+  }
+
+  carregaDados(){
+    this.obterTabelaLogin(this.loginModelDTO.id)
+  }
 
   ngOnInit(): void {
+    this.carregaDados();
+    console.log(this.carregaDados())
+    //this.obterTabelaLogin(id: any)
   }
 
 }
